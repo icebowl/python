5d39f80e066367df5159833c9e262c9ee39d4a3e
Icebowl <icebowl1967@gmail.com>
2017-09-30 06:57:40 -0700

    Add files via upload

---
+++ cyber/print_sfi_functions.py
@@ -0,0 +1,20 @@
+#print_sfi_functions.py
+def intcalc():  
+	a = 3 
+	b = 4
+	print("integers a + b",a+b)
+def floatcalc():
+	a = 3.1
+	b = 4.1
+	print("floats a + b",a+b)
+def stringcat():
+	a = "3.1"
+	b = "4.1"
+	print ("strings",a+" "+b)
+	
+def main():
+	intcalc()
+	floatcalc()
+	stringcat()
+	
+main()
