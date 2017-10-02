6cd329064d29410e73285ab545b3387f7714e673
Icebowl <icebowl1967@gmail.com>
2017-10-02 04:58:29 -0700

    Add files via upload

---
+++ cyber/module1.py
@@ -0,0 +1,22 @@
+print (__name__)
+print ("{}".format(__name__))
+
+#def main():
+#	pass
+#if __name__=='__main__':
+#	main()
+
+
+#https://www.youtube.com/watch?v=sugvnHA7ElY
+#https://www.youtube.com/watch?v=Huz6bS0uLm4
+#https://www.youtube.com/watch?v=JtKOP-ThcbU
+#https://pythonprogramming.net/
+
+# .format
+#https://www.youtube.com/watch?v=vTX3IwquFkc
+
+
+#python tcp
+#https://www.youtube.com/watch?v=WrtebUkUssc
+
+
