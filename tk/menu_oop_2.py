cc08fa95b9f71cc44aa66bf2ad693f86cf0f87dd
icebowl <icebowl1967@gmail.com>
2018-10-19 05:15:10 -0700

    AA

---
+++ tk/menu_oop_2.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+
+class MintApp(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.root = parent
+        self.root.columnconfigure(0, weight = 0)
+        self.root.rowconfigure(0, weight = 0)
+
+
+        main_frame = tk.Frame(self.root, width = 385, height = 460, background = "black")
+        main_frame.columnconfigure(0, weight = 0)
+        main_frame.rowconfigure(0, weight = 0)
+        main_frame.grid(row  = 0, column = 0)
+
+        label_frame = tk.Frame(main_frame, width = 375, height = 115, background = "blue")
+        label_frame.grid(row  = 0, column = 0)
+
+        button_frame = tk.Frame(main_frame, width = 375, height = 330, background = "green")
+        button_frame.grid(row  = 1, column = 0)
+
+def main():
+    root = tk.Tk()
+    MyApp = MintApp(root)
+    tk.mainloop()
+
+if __name__ == '__main__':
+    main()
---
+++ tk/oop_tk_0.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+
+class MainMenu:
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(self.master,width=385, height=460, relief='raised', borderwidth=1, background="#00ff00")
+        #    self.frame = tk.Frame(self.frame, )
+        self.button1 = tk.Button(self.frame, text = 'Choas', width = 25, command = self.new_window)
+        self.button1.pack()
+        self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
+        self.quitButton.pack()
+        self.frame.place(x=100, y=100, anchor="nw", width=400, height=400)
+        self.frame.pack(expand=True, fill='both')
+        
+    def new_window(self):
+        self.newWindow = tk.Toplevel(self.master)
+        self.app = ChoasWindow(self.newWindow)
+    def close_windows(self):
+        self.master.destroy()
+
+class ChoasWindow:
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(self.master)
+        self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
+        self.quitButton.pack()
+        self.frame.pack()
+    def close_windows(self):
+        self.master.destroy()
+
+def main():
+    root = tk.Tk()
+    app = MainMenu(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
