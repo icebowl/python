5e629e7a002c0d490b857edc21c0a165c7c7f815
icebowl <icebowl1967@gmail.com>
2018-12-10 18:45:26 -0800

    AA

---
+++ cidr-0.py
@@ -0,0 +1,17 @@
+import math
+
+def binaryToDecimal(binary):
+    print(binary)
+    binary1 = binary
+    decimal, i, n = 0, 0, 0
+    while(binary != 0):
+        dec = binary % 10
+        decimal = decimal + dec * pow(2, i)
+        binary = binary//10
+        i += 1
+    print(decimal)
+
+# Driver code
+if __name__ == '__main__':
+    subnet = [0,0,0,0]
+    print(subnet)
