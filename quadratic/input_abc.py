794cf6112b6c550e1489f32470941356fa13dd5e
icebowl <icebowl1967@gmail.com>
2019-01-03 05:47:43 -0800

    AA

--- quadratic/input_abc.py
+++ quadratic/input_abc.py
@@ -1,5 +1,7 @@
-from class_quadratic  import * 
+from class_quadratic  import *
+
 def main():
+    print("Input a,b and c from an equation ax^2 + bx + c :")
     p1 = QuadraticEquation(1,0,-9)
     x1 = p1.x1()
     x2 = p1.x2()
