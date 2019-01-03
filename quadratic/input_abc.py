5f2405c7555cae8dd37c9a2635d83c9aedd82e55
icebowl <icebowl1967@gmail.com>
2019-01-03 05:39:57 -0800

    AA

---
+++ quadratic/input_abc.py
@@ -0,0 +1,10 @@
+from class_quadratic  import * 
+def main():
+    p1 = QuadraticEquation(1,0,-9)
+    x1 = p1.x1()
+    x2 = p1.x2()
+    print ("Discriminant = ",p1.discriminant())
+    print ("x1 = ",x1," x2 = ",x2)
+
+if __name__ == "__main__":
+    main()
