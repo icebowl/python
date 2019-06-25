c6504db9c255995ba13dc7313585b28b0ebd3144
icebowl <icebowl1967@gmail.com>
2019-06-25 05:23:44 -0700

    AA

---
+++ edhesive/test-1.8.py
@@ -0,0 +1,9 @@
+num1 = input ("Enter a number: ")
+num2 = input ("Enter a number: ")
+print (num1 + num2)
+
+print(" * * * * * * * * * *")
+num1 = int (input("Enter a number: "))
+num2 = int (input("Enter a number: "))
+num3 = int (input("Enter a number: "))
+print ("The average is: " +str(int(num1 + num2 + num3)/3))
