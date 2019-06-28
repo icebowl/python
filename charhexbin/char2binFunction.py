36b2e101fdffb7e3b5c4bd0e8d917c20f111c84a
icebowl <icebowl1967@gmail.com>
2019-06-28 05:28:03 -0700

    AA

--- charhexbin/char2binFunction.py
+++ charhexbin/char2binFunction.py
@@ -10,12 +10,8 @@ def bincon(word):
  for i in range(0,n):
   b =bin(ord(word[i:i+1]))
   print (word[i:i+1]," ",b)
- #end loop
- print("bincon ",word)
- c1 = word[0:1]
- c2 = word[1:2]
- 
- print("c ",c1,c2)
+  #barray[0] = b
+  #end loop
 
 def main():
  print (len(sys.argv))
