40a02f5f5c730d25a06af40d5328e3d035e0eaf0
icebowl <icebowl1967@gmail.com>
2019-06-28 05:16:49 -0700

    AA

--- charhexbin/char2binFunction.py
+++ charhexbin/char2binFunction.py
@@ -2,17 +2,28 @@
 import sys
 import binascii
 
-def bincon(n):
- print("bincon ",n)
-
+def bincon(word):
+ c = ""
+ b = ""
+ n = len(word)
+ print("n ",n,"\n\n")
+ for i in range(0,n):
+  b =bin(ord(word[i:i+1]))
+  print (word[i:i+1]," ",b)
+ #end loop
+ print("bincon ",word)
+ c1 = word[0:1]
+ c2 = word[1:2]
+ 
+ print("c ",c1,c2)
 
 def main():
  print (len(sys.argv))
  print (str(sys.argv))
- s = sys.argv[1]
- n = len(s)
- bincon(s)
- print(s,n)
+ word = sys.argv[1]
+ n = len(word)
+ bincon(word)
+ print("main ",word,n)
 
 main()
 
---
+++ charhexbin/sub.py
@@ -0,0 +1,9 @@
+x = "Hello World!"
+print(x)
+print(x[:1])
+print(x[2:])
+print(x[:2])
+print(x[:-2])
+print(x[-2:])
+print(x[2:-2])
+
