bfa9bd5d95e109f58648c3299ffdfd0c0d1fb8ca
icebowl <icebowl1967@gmail.com>
2019-09-17 05:19:43 -0700

    AA

---
+++ edhesive/coce.1.7.1.py
@@ -0,0 +1,4 @@
+# your code goes here
+word1 = input("Input word1 :")
+word2 = input("Input word2 :")
+print(word1+" "+word2)
---
+++ edhesive/code.1.7.2.py
@@ -0,0 +1,3 @@
+first = input ('Please input your first name: ')
+last = input ('Please input your last name: ')
+print (last+', '+first)
