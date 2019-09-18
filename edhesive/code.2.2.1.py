0a1ffe6b921cf3cbc45fc0a1b7a3c464954491a2
icebowl <icebowl1967@gmail.com>
2019-09-18 05:21:24 -0700

    AA

---
+++ edhesive/code.2.2.1.py
@@ -0,0 +1,3 @@
+# your code goes here
+n = int(input("Input an integer: "))
+print (str(n+1)+","+str(n+2)+","+str(n+3))
---
+++ edhesive/code2.2.2.py
@@ -0,0 +1,6 @@
+# your code goes here
+a = float(input("input a decimal :"))
+b = float(input("input a decimal :"))
+c = float(input("input a decimal :"))
+sum = a+b+c
+print(str(sum))
