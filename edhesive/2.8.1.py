9b595569d61adfa5e5efa6f7239dc1c1fa3978d8
icebowl <icebowl1967@gmail.com>
2019-09-20 05:16:38 -0700

    AA

---
+++ edhesive/2.8.1.py
@@ -0,0 +1,6 @@
+# your code goes here
+x = int(input("Enter a number: "))
+y = int(input("Enter a number: "))
+z = int(input("Enter a number: "))
+maxvalue = max( x,y,z)
+print ("Largest: " + str(maxvalue))
