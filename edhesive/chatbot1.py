e126daf85748f629d8e597c730f7add15c811d03
icebowl <icebowl1967@gmail.com>
2019-09-23 04:54:06 -0700

    AA

---
+++ edhesive/chatbot1.py
@@ -0,0 +1,21 @@
+'''
+A chatbot is a computer program designed to emulate human conversation. 
+For this program you will use if statements, user input, 
+and random numbers to create a basic chatbot.
+
+Here is the scenario: you have decided to start a website. 
+You are creating a prototype to show investors so you can 
+raise money and launch your website.
+
+You should ask the user at least five questions and use if-elif-else 
+statements and give answers depending on how they answer. 
+Some responses should be based on what they type, 
+and some should be based on random numbers.
+
+For example, if they say they are sad, 
+your chatbot might respond “I’m sorry to hear that”.
+
+You could also have a random number generated between 1 and 3 
+and have a corresponding response depending on the number such as 
+“That is great to hear” or “So interesting”.
+'''
