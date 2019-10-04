7f5e26c9d8343716515bb840473b6b49bea5b84a
icebowl1967 <icebowl1967@gmail.com>
2019-10-04 09:35:18 -0700

    keys.py

---
+++ cyber/keys.py
@@ -0,0 +1,10 @@
+#keys.py Kenny CWC
+key = "0123456789abcdef"
+for i in range (0,16):
+	print(key[i]," ",end="")
+
+h16 = 9
+h1 = 13
+print("\n * * * * * * * ")
+print(9*16+13)
+print(key[9],key[13])
