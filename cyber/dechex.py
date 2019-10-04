b9e0f8a11e6ddfc2b407c6158910f44493b40fc5
icebowl1967 <icebowl1967@gmail.com>
2019-10-04 10:05:47 -0700

    AA

--- cyber/dechex.py
+++ cyber/dechex.py
@@ -1,21 +1,15 @@
-# dechex.py cwc
+# dechex.py cwc kenny
 def hexcon(num):
-	#print(num," in dechex funtion ",end="")
-	h = "";hs16 = "##";hs1="$$" #strings
+	key = "0123456789abcdef" # hex key
+	h = ""
 	h16 = int(num/16)
-	hs16 = str(h16)
-	if(h16 > 9):
-		print("h16 is greater than 9")
 	h1 = num % 16
-	hs1 = str(h1)
-	if(h1 > 9):
-		print("h11 is greater than 9")
-	h = hs16+hs1
+	h = key[h16]+ key[h1]
 	return h
 	
 def main():
 	hs = ""
-	for i in range(147, 160):
+	for i in range(0,256):
 		hs = hexcon(i)
 		print(i,hs)
 	
--- cyber/keys.py
+++ cyber/keys.py
@@ -4,7 +4,7 @@ for i in range (0,16):
 	print(key[i]," ",end="")
 
 h16 = 9
-h1 = 13
+h1 = 0
 print("\n * * * * * * * ")
-print(9*16+13)
-print(key[9],key[13])
+print(9*16+0)
+print(key[9],key[0])
