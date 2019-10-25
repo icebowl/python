d9e0e10550193eacb7c7fd959e01705499963856
icebowl <icebowl1967@gmail.com>
2019-10-25 05:55:11 -0700

    AA

--- pig/chat1.py
+++ pig/chat1.py
@@ -16,21 +16,25 @@ key = ["","","","",""]
 code = ["","","","",""]
 codevalues = [-1,-1,-1,-1,-1]
 #print colors
-print(Fore.CYAN+"CHATBOT PYTHON - CWC",end="")
+print(Fore.CYAN+"CHATBOT PYTHON - CWC")
+print(Fore.CYAN+"ASC2 TABLE CHARACTERS 33 TO 126",end="")
 print(Style.RESET_ALL,end="")
+
 #print asc2 table 33 to 126  cwc
 print(Fore.BLACK,end="")
-print(Back.YELLOW,end="")
+print(Back.YELLOW)
+#print asc2 table 33 to 126
 for i in range (33,127):
     c = chr(i)
     print(str(i)+"="+str(c)+" ",end="")
     if (i % 10 == 0):
         print()
 print(Style.RESET_ALL)
+# create random code values
 for j in range (0,5):
     codevalues[j] = random.randint(0,7)
-print(codevalues)
-#create 
+#print(codevalues) debug
+#create code table
 for j in range (0,5):
     s = ""
     k = 0
@@ -43,13 +47,36 @@ for j in range (0,5):
             k = k + 1
             #print("k ",k)
     key[j] = s
+#get codes
+print(Back.BLUE,end="")
+print(Fore.BLACK+"HERE IS THE CODE MATRIX")
+print(Style.RESET_ALL,end="")
+print(Back.WHITE,end="")
+print(Fore.BLUE,end="")
 for j in range (0,5):
     print(key[j])
     keyvalue = random.randint(0,7)
     temp = key[j]
     temp = temp[codevalues[j]]
     code[j] = temp
-print("\n\n")
-print(code)
-
-
+#print(code) debuf
+print(Back.BLUE,end="")
+print(Fore.BLACK+"TYPE THE CHARACTER CODE THE PRESS Enter . . .")
+print(Style.RESET_ALL)
+check = 0
+for i in range (0,5):
+    row = input("ROW "+str(i+1)+" COLUMN "+str(codevalues[i]+1)+" CHARACTER.")
+    if (row == str(code[i])):
+        check = check + 1
+if(check == 5):
+    print(Fore.BLACK,end="")
+    print("\n\n")
+    print(Back.GREEN+"YOU ARE NOT A ROBOT!")
+    print("\n\n")
+else:
+    print(Fore.BLACK,end="")
+    print("\n\n")
+    print(Back.RED+"ROBOT DETECTED!")
+    print("\n\n")
+print(Style.RESET_ALL)
+print("\n")
