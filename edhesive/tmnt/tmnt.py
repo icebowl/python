25bad9a5353adb8acaf1180b6050fc21bc1f14f5
icebowl <icebowl1967@gmail.com>
2019-11-07 04:44:05 -0800

    AA

---
+++ edhesive/tmnt/tmnt.py
@@ -0,0 +1,10 @@
+from PIL import Image
+with Image.open('mike.png') as img:
+ img.show()
+with Image.open('don.png') as img:
+ img.show()
+with Image.open('ralph.png') as img:
+ img.show()
+with Image.open('leo.png') as img:
+ img.show()
+
