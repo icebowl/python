452e61f2376efd748fe2bd22467b9e557f1c3d13
icebowl <icebowl1967@gmail.com>
2020-02-12 04:33:21 -0800

    AA

---
+++ edhesive/6-test.py
@@ -0,0 +1,12 @@
+
+
+for i in range(11, 31, 1):
+    print(i," ",end="")
+print()
+for i in range(41, 31, -1):
+    print(i," ",end="")
+print()
+for i in range(41, 33, -1):
+    print(i," ",end="")
+print()
+
