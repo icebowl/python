4da5d780f7be03e102733a7c74ea31ee8d7b732c
cwc <icebowl1967@gmail.com>
2020-02-28 15:14:10 -0800

    AA

---
+++ rpi/rgb-led.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+
+REDPIN = 25
+GREENPIN = 24
+BLUE = 23
+
+GPIO.setup(REDPIN, GPIO.OUT)
+GPIO.output(REDPIN,0)
+
+GPIO.setup(GREENPIN, GPIO.OUT)
+GPIO.output(GREENPIN,0)
+
+GPIO.setup(BLUEPIN, GPIO.OUT)
+GPIO.output(BLUEPIN,0)
+
+try:
+	while(True):
+		request = input("RGB-->")
+		if(len(request == 3):
+			GPIO.output(REDPIN,int(request[0])
+			GPIO.output(GREENPIN,int(request[1])
+			GPIO.output(BLUEPIN,int(request[2])
+except Keyboard Interrupt:
+	GPIO.cleanup()
