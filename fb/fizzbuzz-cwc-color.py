b543faedaa7302748e591a562dd3d48e7b0661b2
icebowl <icebowl1967@gmail.com>
2020-02-28 05:25:54 -0800

    AA

--- fb/fizzbuzz-cwc-color.py
+++ fb/fizzbuzz-cwc-color.py
@@ -1,14 +1,24 @@
 # https://pypi.org/project/termcolor/
-import sys
+# pip3 install termcolor --user
+
+import sys, time
 from termcolor import colored
-print (colored('hello', 'red'))
-for n in range(1,101):
-	if n % 3 == 0:
-		print("Fizz",end="")
+
+def main():
+	print (colored('FizzBuzz', 'red'))
+	time.sleep(2)
+	for n in range(1,101):
+		if n % 3 == 0:
+			print("\t\t",end="")
+			print("Fizz",end="")
 		if n%5 != 0:
 			print()
-	if n % 5 == 0:
-		print("Buzz")
-	if n % 3 != 0 and n % 5 != 0:
-		print(n)
+		if n % 5 == 0:
+			print("Buzz")
+		if n % 3 != 0 and n % 5 != 0:
+			print(n)
+		if(n % 10 == 0):
+			time.sleep(2)
 
+if __name__ == '__main__':
+	main()
