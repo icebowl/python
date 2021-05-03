44103fe6cf5ece6affde85e8aeae6f6e796d5e70
cwc <icebowl1967@gmail.com>
2021-05-02 18:23:17 -0700

    AA

---
+++ turtle/testspeed4.py
@@ -0,0 +1,29 @@
+import turtle
+import random
+import time
+screen = turtle.Screen()
+t = turtle.Turtle()
+turtle.tracer(0, 0)
+
+dist = 2
+for i in range(50):
+	t.fd(dist)
+	t.rt(22.5)
+	dist += 0.25
+	#print(dist)
+turtle.update()
+screen.exitonclick()	
+
+
+
+'''
+https://docs.python.org/2/library/turtle.html#turtle.delay
+
+screen.tracer(8, 25)
+>>> dist = 2
+>>> for i in range(200):
+...     fd(dist)
+...     rt(90)
+...     dist += 2
+
+'''
